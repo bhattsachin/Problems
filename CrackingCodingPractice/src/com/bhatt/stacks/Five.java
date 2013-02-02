5c4c71160b26dc935131a8a3cd76bf3cb175e5de
bhatt <bhattsachin@gmail.com>
2013-02-01 23:52:09 -0500

    queue using 2 stacks

---
+++ CrackingCodingPractice/src/com/bhatt/stacks/Five.java
@@ -0,0 +1,80 @@
+package com.bhatt.stacks;
+
+import com.bhatt.linked.Node;
+
+/**
+ * Queue using two stacks
+ * 
+ * @author bhatt
+ * 
+ */
+public class Five {
+
+	Stack first = new Stack();
+	Stack second = new Stack();
+	boolean ispush = true;
+
+	/**
+	 * 
+	 * @param d
+	 */
+	public void enqueue(int d) {
+		Node nd = new Node(d);
+		Node tmp;
+		if (ispush) {
+			first.push(nd);
+		} else {
+			while (second.peek() != null) {
+				tmp = second.pop();
+				first.push(tmp);
+			}
+			// nowaddthedude
+			first.push(nd);
+		}
+		
+		ispush = true;
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int dequeue() {
+		Node tmp;
+		if(!ispush){
+			return second.pop().data;
+		}else{
+			while(first.peek()!=null){
+				tmp = first.pop();
+				second.push(tmp);
+				
+			}
+			
+			ispush = false;
+			return second.pop().data;
+		}
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Five five = new Five();
+		five.enqueue(5);
+		five.enqueue(7);
+		five.enqueue(0);
+		System.out.println(five.dequeue());
+		five.enqueue(89);
+		five.enqueue(23);
+		System.out.println(five.dequeue());
+		System.out.println(five.dequeue());
+		System.out.println(five.dequeue());
+		System.out.println(five.dequeue());
+		
+		
+	}
+
+}
--- CrackingCodingPractice/src/com/bhatt/stacks/Four.java
+++ CrackingCodingPractice/src/com/bhatt/stacks/Four.java
@@ -1,11 +1,36 @@
 package com.bhatt.stacks;
 
+import com.bhatt.linked.Node;
+
 /**
  * Tower of Hanoi
  * @author bhatt
  *
  */
 public class Four {
+	
+	public Stack[] towerofhanoi(Stack[] stacks, int src, int dest){
+		
+		Node nd = stacks[src].pop();
+		if(stacks[src].peek()==null){
+			//this was the only one element in the source
+			//lets put to destination directly
+			stacks[dest].push(nd);
+			return stacks;
+		}else{
+			stacks = towerofhanoi(stacks, 0, 1);
+		}
+		
+		
+		
+		
+		
+		return null;
+		
+		
+	}
+	
+	
 
 	/**
 	 * @param args
