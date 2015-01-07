740c6fae490a896454f5fb8147840cb063ddb82d
bhatt <bhattsachin@gmail.com>
2015-01-07 08:34:55 -0800

    chapter 1

---
+++ src/com/bhatt/algos/exercises/chapter1/Chapter1.java
@@ -0,0 +1,28 @@
+package com.bhatt.algos.exercises.chapter1;
+
+public class Chapter1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println((1+2.236)/2);
+		System.out.println(1+2+3+4.0);
+		System.out.println(4.1>=4);
+		System.out.println(1+2+"3");
+		
+		//1.4
+		int c=4;
+		if(3>4)
+			c=0;
+		else
+			c=1;
+		
+		System.out.println('a');
+		System.out.println('a' + 'b');
+		System.out.println((char)('a'+4));
+		
+		
+		
+
+	}
+
+}
