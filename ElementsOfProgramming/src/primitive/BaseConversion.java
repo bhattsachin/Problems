35502163762bd0ee463729b3c89e0ae0918e9cfc
bhatt <bhattsachin@gmail.com>
2015-03-23 23:10:17 -0700

    primitive

---
+++ ElementsOfProgramming/src/primitive/BaseConversion.java
@@ -0,0 +1,13 @@
+package primitive;
+
+/**
+ * Created by bhatt on 3/23/15.
+ */
+public class BaseConversion {
+
+    public static void main(String args[]){
+        System.out.println("Hello");
+        System.out.print(81>>3);
+
+    }
+}
