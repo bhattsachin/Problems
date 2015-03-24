7cfa94d41604f5b79cfac803695b643dc70df525
bhatt <bhattsachin@gmail.com>
2015-03-23 23:22:35 -0700

    base

--- ElementsOfProgramming/src/primitive/BaseConversion.java
+++ ElementsOfProgramming/src/primitive/BaseConversion.java
@@ -2,12 +2,40 @@ package primitive;
 
 /**
  * Created by bhatt on 3/23/15.
+ * Convert String from base 1 to base 2
  */
 public class BaseConversion {
 
     public static void main(String args[]){
-        System.out.println("Hello");
-        System.out.print(81>>3);
+        System.out.println(numVal('c'));
+        assert(4==5);
+    }
 
+    public static String convertBase(int base1, String baseString, int base2){
+        assert(base1<=16 && base2<=16);
+        int numInBase10=0;
+        for(char c : baseString.toCharArray()){
+            
+        }
+
+        return "ok";
+    }
+
+
+    /**
+     * convert a char value to max hex decimal value
+     * @param c
+     * @return
+     */
+    public static int numVal(char c){
+        if(c>=48 && c<=57){
+            return c-48;
+        }
+
+        if(c>=97 && c<=102){
+            return c-97+10;
+        }
+
+        return 0;
     }
 }
